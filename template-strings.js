1429b51ccb598c5ed6a627e3c99175c82c92b20d
Maurício Pio <arquitetomauricio@hotmail.com>
2022-08-06 13:49:19 -0300

    opredores e funções

---
+++ template-strings.js
@@ -0,0 +1,8 @@
+const nome = "Maurício";
+const idade = 2022 - 1986;
+const cidadeDeNascimento = "Brasília";
+// const apresentacao = "Meu nome é " + nome ;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
